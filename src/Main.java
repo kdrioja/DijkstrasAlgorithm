d4f9d68e9cd38d57df0a128b4d598830c74aca61
Kenia Rioja <kdrioja@gmail.com>
2019-05-09 11:56:42 -0700

    Can now generate edges with weights determined by probability

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,16 @@ public class Main {
     public static void main(String[] args) {
         int[] nodes = generateNodes();
         printNodes(nodes);
-        Random rand = new Random();
+        ArrayList<Edge> edges = generateEdges(nodes.length);
+        printEdges(edges);
+
+
+
+
 
         for (int i = 0; i < 100; i++) {
             //System.out.println(rand.nextDouble());
-            System.out.println(generateWeight(5));
+            //System.out.println(generateWeight(5));
         }
     }
 
@@ -28,30 +33,14 @@ public class Main {
         int[] result = new int[n];
 
         for (int i = 1; i <= n; i++) {
-            result[i-1] = i;
+            result[i - 1] = i;
         }
         return result;
     }
 
-    public static void printNodes(int[] nodes) {
-        System.out.print("Node list: {");
-
-        for (int i = 0; i < nodes.length - 1; i++) {
-            System.out.print(nodes[i] + ", ");
-        }
-        System.out.println(nodes[nodes.length - 1] + "}");
-    }
-
-    public static void printEdges(ArrayList<Edge> edges) {
-        System.out.print("Edge list: {");
-
-        for (int i = 0; i < edges.size(); i++) {
-            
-        }
-    }
 
     public static ArrayList<Edge> generateEdges(int n) {
-        double probability = 0.25;
+        double probability = 0.50;
         Random rand = new Random();
         ArrayList<Edge> edges = new ArrayList<>();
 
@@ -65,9 +54,31 @@ public class Main {
         return edges;
     }
 
+
     public static int generateWeight(int n) {
         int min = 1, max = 5 * n;
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
+    }
+    
+
+    public static void printNodes(int[] nodes) {
+        System.out.print("Node list: {");
+
+        for (int i = 0; i < nodes.length - 1; i++) {
+            System.out.print(nodes[i] + ", ");
+        }
+        System.out.println(nodes[nodes.length - 1] + "}");
+    }
+
+
+    public static void printEdges(ArrayList<Edge> edges) {
+        System.out.print("Edge list: {");
+
+        for (int i = 0; i < edges.size() - 1; i++) {
+            System.out.print(edges.get(i) + ", ");
+        }
+
+        System.out.println(edges.get(edges.size() - 1) + "}");
     }
 }
