42e991e3c614dabd61d64649656fbc31ee849705
Kenia Rioja <kdrioja@gmail.com>
2019-05-09 10:11:59 -0700

    Can create node list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int[] nodes = generateNodes();
+        printNodes(nodes);
+    }
 
+    public static int[] generateNodes() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+
+        while (n <= 0) {
+            System.out.println("Integer entered was not positive.");
+            System.out.print("Enter a positive integer: ");
+            n = scanner.nextInt();
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 1; i <= n; i++) {
+            result[i-1] = i;
+        }
+        return result;
+    }
+
+    public static void printNodes(int[] nodes) {
+        System.out.print("Node list: {");
+
+        for (int i = 0; i < nodes.length - 1; i++) {
+            System.out.print(nodes[i] + ", ");
+        }
+        System.out.println(nodes[nodes.length - 1] + "}");
     }
 }
